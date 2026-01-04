8c79388e169c2b94b787953eb941c6e5bc5d435d
Adi Soumitra Mondal <adithecomputerguy@gmail.com>
2026-01-04 19:20:43 +0000

    added folder src and moved Program.cs and blueprint-repo.csproj into it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// The project files have been moved into the `src/` folder.
+// See src/Program.cs for the application's entry point.
